627539d1d17a29a97ea6abf4076590d580b0d89d
buchijiucai123 <104132805+buchijiucai123@users.noreply.github.com>
2022-04-25 16:46:46 +0800

    轮播图获取

--- src/components/swiper/SwiperCom.vue
+++ src/components/swiper/SwiperCom.vue
@@ -12,15 +12,19 @@
 <script>
     import 'swiper/css/swiper.min.css'
     import Swiper from 'swiper'
+
+    import {getBanner} from "@/api";
+
     export default {
         name: "SwiperCom",
         data(){
             return{
-                imgs:[//这个用法不标准，网页上的图片不应该是前端本地的图，应该是来自于后端
-                    {pic:require('../../assets/img/swiper1.jpg'),id:0},
-                    {pic:require('../../assets/img/swiper2.jpg'),id:1},
-                    {pic:require('../../assets/img/swiper3.png'),id:2},
-                ]
+                // imgs:[//这个用法不标准，网页上的图片不应该是前端本地的图，应该是来自于后端
+                //     {pic:require('../../assets/img/swiper1.jpg'),id:0},
+                //     {pic:require('../../assets/img/swiper2.jpg'),id:1},
+                //     {pic:require('../../assets/img/swiper3.png'),id:2},
+                // ]
+                imgs:[]
             }
         },
         mounted(){
@@ -30,8 +34,19 @@
                 //引号里面可以用id名 class名 皆可，只要能找到对应的container就行
                 loop:true,
                 autoplay:true// swiper自动滑动
-
             })
+
+        },
+        created(){
+            this.getBannerImgs()
+            //创建之后向后台要数据，imgs[]为空数组
+        },
+        methods:{
+              async getBannerImgs(){//异步
+                const res=await getBanner(1)//跟后台要安卓端的图片 数据
+                  this.imgs=res.data.banners//将跟后台要到的数据，赋值给本对象的data
+
+            }
         }
     }
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,20 @@
 <template>
 <h1>发现</h1>
     <SwiperCom></SwiperCom>
+    <IconList></IconList>
+    <MusicList></MusicList>
 </template>
 
 <script>
 
 import SwiperCom from "@/components/swiper/SwiperCom";
+import IconList from "@/components/icon-list/IconList";
+import MusicList from "@/components/music-list/MusicList";
+
 export default {
   name: 'HomeView',
   components: {
-    SwiperCom
+    SwiperCom,IconList,MusicList
   }
 }
 </script>
